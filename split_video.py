be33634385b20c625ec9bb2d1418886595c29d16
olotem-huji <omer.lotem@mail.huji.ac.il>
2025-06-18 17:23:56 +0300

    Split video to frames

---
+++ split_video.py
@@ -0,0 +1,23 @@
+import os
+import cv2
+
+
+def split_video_to_frames(path, rot=-1):
+    video_name = path.split("\\")[-1].split(".")[0]
+    cap = cv2.VideoCapture(path)
+    i = 0
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
+        i += 1
+        print(i)
+        if rot != -1:
+            frame = cv2.rotate(frame, rot)
+        os.makedirs(fr'./Outputs/{video_name}/Frames/', exist_ok=True)
+        cv2.imwrite(fr'./Outputs/{video_name}/Frames/Frame_{i}.jpg', frame)
+    cap.release()
+
+
+video_path = r"C:\Physics\Year 2\Lab\Advanced Lab 2B\21.05\PXL_20250521_053352973.mp4"
+split_video_to_frames(video_path, rot=cv2.ROTATE_90_CLOCKWISE)
